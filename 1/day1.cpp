e044db34055f38d1c7eb16b1aaa18d2383c96340
Daniel <dm858@york.ac.uk>
2020-12-10 02:36:21 +0100

    Created AoC2015, finished day 1

---
+++ 1/day1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"utils.h"
+
+int main(){
+
+    // read input into vector of strings.
+    std::vector<std::string> input_full = read_input("input", "");
+
+    // exctract single line into string
+    std::string input = input_full[0];
+
+    // current floor
+    int floor = 0;
+    bool got_to_basement = false;
+
+    // work through instructions
+    for ( int i=0; i<input.size(); i++){
+
+        // Part 1 
+        if ( input[i] == '(' ){
+            floor++;
+        }
+        else {
+            floor--;
+        }
+
+        // Part 2
+        if ( (floor < 0) && !got_to_basement ){
+            std::cout << "Got to basement after " << i+1 << " steps." << std::endl;
+            got_to_basement = true;
+        }
+
+
+    }
+
+    // output final floor
+    std::cout << "Final floor: " << floor << std::endl;
+
+
+    return 0;
+}
