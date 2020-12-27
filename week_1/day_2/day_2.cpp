2b2c7a55bb09c6f90bef8ad08c8245ca5c8e88e9
Daniel <daniel.mlak@gmail.com>
2020-12-27 21:24:28 +0100

    Started day 2

--- week_1/day_2/day_2.cpp
+++ week_1/day_2/day_2.cpp
@@ -8,7 +8,7 @@
 int main(){
 
     // read input into vector of strings.
-    std::vector<std::string> input = read_input("input", "");
+    std::vector<std::vector<int>> input = input_to_int_2D(read_input_2D("input", "x"));
 
     return 0;
 }
