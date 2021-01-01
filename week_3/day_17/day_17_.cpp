d88a69d7e4ce46788af1359b6bb1da753b4bfabb
Daniel <daniel.mlak@gmail.com>
2021-01-01 17:38:08 +0100

    Finished day 17

--- week_3/day_17/day_17_.cpp
+++ week_3/day_17/day_17_.cpp
@@ -14,13 +14,19 @@ int main(){
 
     std::sort(input.rbegin(), input.rend());
     std::vector<int> best(target+1), ways(target+1);
+    std::fill(best.begin(), best.end(), std::numeric_limits <int>::max() - 1);
 
     ways[0] = 1;
-    //best[0] = 0;
+    best[0] = 0;
 
     for (auto && c : input){
         for (int rem=target; rem >= c; --rem){
-            ways[rem] += ways[rem-c];
+            if (best[rem - c] + 1 < best[rem]){
+                best[rem] = best[rem-c] + 1, ways[rem] = 0;
+            }
+            if (best[rem - c] + 1 == best[rem]){
+                ways[rem] += ways [rem - c];
+            }
         }
     }
 
