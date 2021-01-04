11d0b2212ef994e036a43d031c56fc42014e29f0
Daniel <daniel.mlak@gmail.com>
2021-01-04 17:29:29 +0100

    Small improvements to new_day.sh

--- new_day.sh
+++ new_day.sh
@@ -14,7 +14,11 @@ echo "#include<iostream>
 int main(){
 
     // read input into vector of strings.
-    std::vector<std::string> input = read_input(\"input\", \"\");
+    // std::vector<std::string> input = read_input(\"input\", \"\");
+
+    // read input into vector of vector of strings.
+    // std::vector<std::string> delimiters = {};
+    // std::vector<std::vector<std::string>> input = read_input_2D(\"input\", delimiters);
 
     std::cout << \"Answer (part 1): \" << std::endl;
     std::cout << \"Answer (part 2): \" << std::endl;
@@ -30,9 +34,6 @@ echo "#include<iostream>
 #include\"../../Utils/utils.h\"
 
 int main(){
-
-    // read input into vector of strings.
-    //std::vector<std::string> input = read_input(\"input\", \"\");
 
     return 0;
 }" >> day_${1}/test.cpp
