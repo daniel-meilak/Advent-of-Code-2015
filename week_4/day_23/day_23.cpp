77db2e214997d94aaa9a0f93ae5ce8278f416e67
Daniel <daniel.mlak@gmail.com>
2021-01-04 04:22:20 +0100

    Finished day 23

---
+++ week_4/day_23/day_23.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"../../Utils/utils.h"
+
+// forward function declarations
+void computer(unsigned int &a, unsigned int &b , std::vector<std::vector<std::string>> &input);
+
+int main(){
+
+    // read input into vector of vector of strings.
+    std::vector<std::string> delimiter = {" ", ",", "+"};
+    std::vector<std::vector<std::string>> input = read_input_2D("input", delimiter);
+
+    unsigned int a1 = 0, a2 = 1;
+    unsigned int b1 = 0, b2 = 0;
+
+    computer(a1,b1,input);
+    computer(a2,b2,input);
+
+    std::cout << "Answer (part 1): " << b1 << std::endl;
+    std::cout << "Answer (part 2): " << b2 << std::endl;
+
+    return 0;
+}
+
+void computer(unsigned int &a, unsigned int &b , std::vector<std::vector<std::string>> &input){
+
+    for (unsigned int i=0; i<input.size(); i++){
+
+        std::string &instr = input[i][0];
+        std::string &reg   = input[i][1];
+
+        if (instr == "hlf"){
+            if (reg == "a"){ a /= 2; }
+            else {b /= 2; }
+        }
+        else if ( instr == "tpl"){
+            if (reg == "a"){ a *= 3; }
+            else {b *= 3; }
+        }
+        else if ( instr == "inc"){
+            if (reg == "a"){ a++; }
+            else { b++; }
+        }
+        else if ( instr == "jmp"){
+            i += std::stoi(reg) - 1;    
+        }
+        else if ( instr == "jie"){
+            if (reg == "a"){
+                if (a % 2 == 0){
+                    i += std::stoi(input[i][2]) - 1;
+                }
+            }
+            else {
+                if (b % 2 == 0){
+                    i += std::stoi(input[i][2]) - 1;
+                }
+            }
+        }
+        else if ( instr == "jio"){
+            if (reg == "a"){
+                if (a == 1){
+                    i += std::stoi(input[i][2]) - 1;
+                }
+            }
+            else {
+                if (b == 1){
+                    i += std::stoi(input[i][2]) - 1;
+                }
+            }
+        }
+    }
+}
