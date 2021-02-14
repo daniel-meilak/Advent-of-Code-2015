ce77c58c61454665d13f572cc804665d45b16ece
Daniel <daniel.mlak@gmail.com>
2021-02-15 00:43:10 +0100

    Refactored week 3

--- week_3/day_18/day_18.cpp
+++ week_3/day_18/day_18.cpp
@@ -6,25 +6,33 @@
 #include"../../Utils/utils.h"
 
 // forward function declarations
-int check_neighbours(const std::vector<std::vector<bool>> &grid, unsigned int x, unsigned int y);
+int check_neighbours(const std::vector<std::vector<bool>> &grid, size_t x, size_t y);
 int count_lights(const std::vector<std::vector<bool>> &grid);
 void corners_on(std::vector<std::vector<bool>> &grid);
+int lights(const std::vector<std::string> &input, const bool part2);
 
 int main(){
 
     // read input into vector of strings.
     std::vector<std::string> input = read_input("input", "");
 
+    std::cout << "Answer (part 1): " << lights(input,false) << std::endl;
+    std::cout << "Answer (part 2): " << lights(input,true ) << std::endl;
+
+    return 0;
+}
+
+int lights(const std::vector<std::string> &input, const bool part2){
+    
     // gird of lights (booleans)
     std::vector<std::vector<bool>> grid(input.size()+2,std::vector<bool>(input.size()+2,false));
 
-
     // build initial grid
-    int input_size = input.size();
-    for (int i=0; i<input_size; i++){
+    size_t input_size = input.size();
+    for (size_t i=0; i<input_size; i++){
         
-        int line_size = input[i].size();
-        for (int j=0; j<line_size; j++){
+        size_t line_size = input[i].size();
+        for (size_t j=0; j<line_size; j++){
 
             // if input[i][j] is on (#), set grid to true
             grid[i+1][j+1] = (input[i][j] == '#');
@@ -32,47 +40,39 @@ int main(){
     }
 
     // for part two, turn on the corners
-    corners_on(grid);
+    if (part2){ corners_on(grid); }
 
     // copy of grid
     std::vector<std::vector<bool>> grid_copy = grid;
 
     // perform steps
     int steps = 100;
-    int grid_size = grid.size()-1;
+    size_t grid_size = grid.size()-1;
     for (int i=0; i<steps; i++){
 
-        for (int y=1; y<grid_size; y++){
+        for (size_t y=1; y<grid_size; y++){
 
-            int line_size = grid[y].size()-1;
-            for (int x=1; x<line_size; x++){
+            size_t line_size = grid[y].size()-1;
+            for (size_t x=1; x<line_size; x++){
 
                 // check neighbours
                 int num_neighbours = check_neighbours(grid_copy,x,y);
 
-                if ( grid_copy[y][x] ){
-                    if (num_neighbours < 2 || num_neighbours > 3){ grid[y][x] = false; }
-                }
-                else {
-                    if (num_neighbours == 3){ grid[y][x] = true; }
-                }
+                if (grid_copy[y][x] && (num_neighbours < 2 || num_neighbours > 3)){ grid[y][x] = false; }
+                else if (num_neighbours == 3){ grid[y][x] = true; }
             }
         }
         
-        // keep corners on
-        corners_on(grid);
+        // keep corners on for part2
+        if (part2){ corners_on(grid); }
         grid_copy = grid;
     }
 
     // count number of on lights
-    int on = count_lights(grid);
-
-    std::cout << "Answer: " << on << std::endl;
-
-    return 0;
+    return count_lights(grid);
 }
 
-int check_neighbours(const std::vector<std::vector<bool>> &grid, unsigned int x, unsigned int y){
+int check_neighbours(const std::vector<std::vector<bool>> &grid, size_t x, size_t y){
 
     int count = 0;
 
@@ -92,8 +92,8 @@ int count_lights(const std::vector<std::vector<bool>> &grid){
 
     int count = 0;
 
-    int grid_size = grid.size()-1;
-    for (int y=1; y<grid_size; y++){
+    size_t grid_size = grid.size()-1;
+    for (size_t y=1; y<grid_size; y++){
 
         int line_size = grid[y].size()-1;
         for (int x=1; x<line_size; x++){
@@ -106,7 +106,7 @@ int count_lights(const std::vector<std::vector<bool>> &grid){
 
 void corners_on(std::vector<std::vector<bool>> &grid){
 
-    int n = grid.size()-2;
+    size_t n = grid.size()-2;
 
     grid[1][1] = true;
     grid[n][1] = true;
--- week_3/day_20/README.md
+++ week_3/day_20/README.md
@@ -1,5 +1,1 @@
 ## Day 20
-
-day_20.cpp contains my original implementation with a clever factor finding algorithm which recognises that every factor found comes in a pair (i & x/i). day_20v2.cpp is written using the pseudocode presented in the solutions page for [AoC 2015 day 20](https://www.reddit.com/r/adventofcode/comments/3xjpp2/day_20_solutions/cy59zd9?utm_source=share&utm_medium=web2x&context=3). 
-
-v2 is a much faster implementation (around 10x) which sacrifices more memory usage for a more efficient algorithm. 
