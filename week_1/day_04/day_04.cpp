4e6c90fead77d466df17a4b50a38275fcbe5e9fa
Daniel Meilak <daniel.mlak@gmail.com>
2022-01-07 00:16:53 +0100

    added openssl submodule for day 4

--- week_1/day_04/day_04.cpp
+++ week_1/day_04/day_04.cpp
@@ -4,8 +4,23 @@
 #include<climits>
 #include<algorithm>
 #include<cstdlib>
+#include<sstream>
+#include<iomanip>
 #include"../../Utils/utils.h"
-#include"md5.h"
+#include<openssl/md5.h>
+
+std::string get_md5(const std::string& input){
+    
+    unsigned char result[MD5_DIGEST_LENGTH];
+    MD5(reinterpret_cast<const unsigned char*>(input.c_str()), input.size(), result);
+
+    std::ostringstream sout;
+    sout << std::hex << std::setfill('0');
+    for(long long c: result){ sout << std::setw(2) << (long long)c; }
+
+    return sout.str();
+}
+
 
 int main(){
 
@@ -19,7 +34,7 @@ int main(){
 
         hash = input + std::to_string(i);
 
-        output = MD5(hash).hexdigest();
+        output = get_md5(hash);
 
         if (part1.empty() && output.substr(0,5) == seq1){ part1 = hash; }
         if (output.substr(0,6) == seq2 ){ part2 = hash; }
